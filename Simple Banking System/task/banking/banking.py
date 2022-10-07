718391dbab4014dee5ed1a29b4312b113e1b1c6c
Robbie McDowall <robbie.mcdowall@gmail.com>
2022-10-07 15:17:53 +1000

    Robbie McDowall - added transactions

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -33,6 +33,9 @@ SELECT_ACCOUNT_QUERY = """
 SELECT id, number, pin, balance FROM card WHERE number = ?; 
 """
 
+DELETE_ACCOUNT_QUERY = """
+DELETE FROM card WHERE number = ?; 
+"""
 
 conn: Connection = None
 
@@ -66,9 +69,14 @@ class Account:
     def __init__(self, account_number=None, pin=None, balance=0, persist=True):
         self.pin = pin
         self.account_number = account_number
-        self.balance = balance
+        self._balance = balance
         if persist:
             self.save_to_db()
+
+    @property
+    def balance(self):
+        self._balance = Account.get_account_from_db(self.card_number)._balance
+        return self._balance
 
     @property
     def card_number(self):
@@ -95,12 +103,50 @@ class Account:
                 return None
 
     def save_to_db(self):
-        with contextlib.closing(conn.cursor()) as cur:
-            if self.get_account_from_db(self.account_number):
-                cur.execute(UPDATE_ACCOUNT_STATEMENT, (self.pin, self.balance, self.card_number))
-            else:
-                cur.execute(INSERT_ACCOUNT_STATEMENT, (self.card_number, self.pin))
-            conn.commit()
+        with conn:
+            with contextlib.closing(conn.cursor()) as cur:
+                if self.get_account_from_db(self.account_number):
+                    cur.execute(UPDATE_ACCOUNT_STATEMENT, (self.pin, self.balance, self.card_number))
+                else:
+                    cur.execute(INSERT_ACCOUNT_STATEMENT, (self.card_number, self.pin))
+
+    def close(self):
+        with conn:
+            with contextlib.closing(conn.cursor()) as cur:
+                cur.execute(DELETE_ACCOUNT_QUERY, (str(self.card_number),))
+            print('The account has been closed!')
+
+    def add_income(self):
+        amount = float(input('Enter income'))
+        with conn:
+            with contextlib.closing(conn.cursor()) as cur:
+                cur.execute(UPDATE_ACCOUNT_STATEMENT, (self.pin, self.balance + amount, self.card_number))
+
+    def transfer(self):
+        print('Transfer')
+        recipient_card_number = input('Enter card number:')
+        if recipient_card_number == self.card_number:
+            print("You can't transfer money to the same account!")
+            return
+
+        if not is_valid(recipient_card_number):
+            print('Probably you made a mistake in the card number. Please try again!')
+            return
+
+        recipient = Account.get_account_from_db(recipient_card_number)
+        if not recipient:
+            print('Such a card does not exist.')
+            return
+
+        amount = float(input('Enter how much money you want to transfer:'))
+        if amount > self.balance:
+            print('Not enough money!')
+            return
+
+        with conn:
+            with contextlib.closing(conn.cursor()) as cur:
+                cur.execute(UPDATE_ACCOUNT_STATEMENT, (self.pin, self.balance - amount, self.card_number))
+                cur.execute(UPDATE_ACCOUNT_STATEMENT, (recipient.pin, recipient.balance + amount, recipient.card_number))
 
 
 def is_valid(card_number_input):
@@ -108,10 +154,14 @@ def is_valid(card_number_input):
 
 
 def login() -> Account:
-    card_number_input = input('Enter your card number:')
-    pin_input = input('Enter your PIN:')
-    found_account = Account.get_account_from_db(card_number_input)
-    if not is_valid(card_number_input) or not found_account or pin_input != found_account.pin:
+    """
+    Captures card number and pin from input and attempts to fetch account
+    :return: Account with matching card number and PIN
+    """
+    card_number = input('Enter your card number:')
+    pin = input('Enter your PIN:')
+    found_account = Account.get_account_from_db(card_number)
+    if not is_valid(card_number) or not found_account or pin != found_account.pin:
         print('Wrong card number or PIN!')
         return None
 
@@ -126,36 +176,35 @@ def start():
         cur.execute(CREATE_TABLE_STATEMENT)
         conn.commit()
 
-
     while True:
-        print("""
-1. Create an account
-2. Log into account
-0. Exit
-""")
+        print("1. Create an account\n2. Log into account\n0. Exit\n")
         user_input = input()
         if user_input == '1':
             account = Account.generate_new_account()
-            print(f"""Your card has been created
-Your card number:
-{account.card_number}
-Your card PIN:
-{account.pin}
-""")
+            print(f"Your card has been created\nYour card number:\n{account.card_number}\nYour card PIN:\n{account.pin}\n")
         elif user_input == '2':
             account = login()
             while account:
-                print("""1. Balance
-2. Log out
-0. Exit
-""")
+                print("1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n")
                 user_input = input()
                 if user_input == '1':
                     print(f'Balance: {account.balance}')
                 elif user_input == '2':
+                    # add income
+                    account.add_income()
+                elif user_input == '3':
+                    # do transfer
+                    account.transfer()
+                elif user_input == '4':
+                    # close account
+                    account.close()
+
+                elif user_input == '5':
+                    # logout
                     print('You have successfully logged out!')
                     break
                 elif user_input == '0':
+                    # exit
                     return
                 else:
                     print('Input not valid. Must be 1 (balance), 2 (log out) or 0 (exit)')
